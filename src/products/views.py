d7e96566509a1f0ad62ac5401885a873bc3735d8
jude <jude@gogame.net>
2016-03-19 23:05:49 +0800

    create not found html and added in validation of user logging in to system

--- src/products/views.py
+++ src/products/views.py
@@ -6,23 +6,28 @@ from .models import Product
 
 
 def details_view(request):
-    print request
-
-    product = Product.objects.all().first()
-    template = "detail_view.html"
-    context = {
-        "title": "Detail view",
-        "product": product.title
-    }
+    if request.user.is_authenticated():
+        product = Product.objects.all().first()
+        template = "detail_view.html"
+        context = {
+            "title": "Detail view",
+            "product": product.title
+        }
+    else:
+        template = "not_found.html"
+        context = {}
     return render(request, template, context)
 
 
 def list_view(request):
-    print request
-    queryset = Product.objects.all()
-    template = "list_view.html"
-    context = {
-        "title": "list view",
-        "queryset": queryset
-    }
+    if request.user.is_authenticated():
+        queryset = Product.objects.all()
+        template = "list_view.html"
+        context = {
+            "title": "list view",
+            "queryset": queryset
+        }
+    else:
+        template = "not_found.html"
+        context = {}
     return render(request, template, context)

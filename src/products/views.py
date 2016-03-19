1b6d0b3f9081609b666c9e6b97dd3fd9c6f3c70b
jude <jude@gogame.net>
2016-03-19 22:42:17 +0800

    changed template to consume product information

--- src/products/views.py
+++ src/products/views.py
@@ -1,19 +1,28 @@
 from django.shortcuts import render
 
-
 # Create your views here.
+
+from .models import Product
+
 
 def details_view(request):
     print request
 
+    product = Product.objects.all().first()
     template = "detail_view.html"
-    context = {}
+    context = {
+        "title": "Detail view",
+        "product": product.title
+    }
     return render(request, template, context)
 
 
 def list_view(request):
     print request
-
+    queryset = Product.objects.all()
     template = "list_view.html"
-    context = {}
+    context = {
+        "title": "list view",
+        "queryset": queryset
+    }
     return render(request, template, context)

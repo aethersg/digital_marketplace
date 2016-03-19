fab2349b4e348cd8ab7f62b22278417e4922b49a
jude <jude@gogame.net>
2016-03-19 22:07:20 +0800

    changed admin for input of information

--- src/products/admin.py
+++ src/products/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["__unicode__", "description", "price", "sale_price"]
+    search_fields = ["price", "description"]
+    list_filter = ["price"]
+    list_editable = ["price", "sale_price"]
+
+    class Meta:
+        model = Product
+
+
+admin.site.register(Product, ProductAdmin)
--- src/products/models.py
+++ src/products/models.py
@@ -4,8 +4,11 @@ from django.db import models
 
 
 # Create your models here.
-class Products(models.Model):
+class Product(models.Model):
     title = models.CharField(max_length=50)
+    description = models.TextField(default='Default value')
+    price = models.DecimalField(decimal_places=2, max_digits=100, default=0.99)
+    sale_price = models.DecimalField(max_digits=100, decimal_places=2, default=0.99, null=True, blank=True)
 
     def __unicode__(self):
         return self.title

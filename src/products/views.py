0c058cca923a79c3b9ad81d58e9ec775f5e0ee2b
jude <jude@gogame.net>
2017-05-01 22:38:41 +0800

    added in new model for slug field

--- src/products/views.py
+++ src/products/views.py
@@ -6,7 +6,32 @@ from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
-def details_view(request, object_id):
+def details_slug_view(request, slug=None):
+
+    if request.user.is_authenticated():
+        if slug is not None:
+            try:
+                try:
+                    product = get_object_or_404(Product, slug=slug)
+                except Product.MultipleObjectsReturned:
+                    product = Product.objects.filter(slug=slug).order_by('title').first()
+            except ValueError:
+                raise Http404
+            template = "detail_view.html"
+            context = {
+                "title": "Detail view",
+                "product": product.title
+            }
+            return render(request, template, context)
+        else:
+            raise Http404
+    else:
+        template = "not_found.html"
+        context = {}
+    return render(request, template, context)
+
+
+def details_view(request, object_id=None):
     if request.user.is_authenticated():
         if object_id is not None:
             try:

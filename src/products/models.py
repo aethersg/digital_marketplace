132f7635f184267512df6e34105a88b9fa82b58a
jude <jude@gogame.net>
2017-05-01 22:38:21 +0800

    added in new model for slug field

--- src/products/models.py
+++ src/products/models.py
@@ -6,6 +6,7 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     title = models.CharField(max_length=50)
+    slug = models.SlugField(default='slug-field')  # unique=True
     description = models.TextField(default='Default value')
     price = models.DecimalField(decimal_places=2, max_digits=100, default=0.99)
     sale_price = models.DecimalField(max_digits=100, decimal_places=2, default=0.99, null=True, blank=True)
